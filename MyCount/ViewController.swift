fa6dd426ddf2a112e4e30a70142feb8007b50593
Dudi001 <murad.manapov.96@gmail.com>
2022-11-30 19:35:42 +0300

    Добавил счетчик, кнопку и реализовал возрастание счетчика на 1

--- MyCount/ViewController.swift
+++ MyCount/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    private var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func actionCount(_ sender: Any) {
+            count += 1
+            countLabel.text = "Значение счетчика: \(count)"
+            
+    }
 }
-
